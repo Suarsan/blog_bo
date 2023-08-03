590cb83c1b60cac857f84f10a0ddd69ed1548f32
Javier Suárez <javiersuarsan@gmail.com>
2023-08-02 20:40:38 -0400

    [feature] delete unused code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  restURL: 'http://localhost:5000/',
   apiURL: 'https://api.descubrelavera.com',
   frontURL: 'https://descubrelavera.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 
 export const environment = {
   production: false,
-  restURL: 'http://localhost:5000/',
   apiURL: 'http://localhost:5000/graphql',
   // apiURL: 'https://api.descubrelavera.com/',
   frontURL: 'https://descubrelavera.com/'
