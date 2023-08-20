e81df0bbfa3e714fe3bea46dad0ad23075eb3a94
Javier Suárez <javiersuarsan@gmail.com>
2023-08-20 11:17:16 -0400

    [feature] improve search behaviour

--- src/app/pipes/filter-by.pipe.ts
+++ src/app/pipes/filter-by.pipe.ts
@@ -5,9 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterByPipe implements PipeTransform {
 
-  transform(values: any[], args: any): unknown {
-    if (values && args) {
-      return values.filter(v => RegExp(args).test(v.title));
+  transform(values: any[], args: any[]): unknown {
+    if (values && args?.[0]) {
+      return values.filter(v => 
+        args[1].some(attr => { return RegExp(args[0]).test(v[attr])})
+      );
     }
     return values;
   }
